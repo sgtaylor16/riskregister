18294e1b7a59288edecdcde2db09d3f13f6659d6
Scott Taylor <sgtaylor16@gmail.com>
2025-05-06 20:29:22 -0400

    Old db setup works

--- dbcode/models.py
+++ dbcode/models.py
@@ -11,38 +11,38 @@ class Persons(db.Model):
     id: Mapped[int] = Column(Integer, primary_key=True)
     last_name: Mapped[str] = Column(String(50),nullable=False)
     first_name: Mapped[str] = Column(String(50),nullable=False)
-    risks: Mapped[List["Risks"]] = relationship("Risks",backref="person")
+    risks: Mapped[List["Risks"]] = relationship("Risks",back_populates="person")
 
 class Risks(db.Model):
     __tablename__ = 'risks'
-    id: Mapped[int] = Column(primary_key=True)
+    id: Mapped[int] = Column(Integer,primary_key=True)
     ifstatement: Mapped[str] = Column(String(100))
     thenstatement: Mapped[str] = Column(String(100))
     probability: Mapped[int] = Column(Integer)
     impact: Mapped[int] = Column(Integer)
     person_id: Mapped[int] = Column(ForeignKey('persons.id'),nullable=True)
-    person: Mapped['Persons'] = relationship("Persons",backref="risks")
+    person: Mapped['Persons'] = relationship("Persons",back_populates="risks")
     program_id: Mapped[int] = Column(ForeignKey('programs.id'))
-    program: Mapped["Programs"] = relationship("Programs",backref="risks")
-    mitigations: Mapped[List["Mitigations"]] = relationship("Mitigations",backref="risk")
+    program: Mapped["Programs"] = relationship("Programs",back_populates="risks")
+    mitigations: Mapped[List["Mitigations"]] = relationship("Mitigations",back_populates="risk")
     date: Mapped[datetime] = Column(DateTime)
     realizedate: Mapped[datetime] = Column(DateTime,nullable=True)
     expiredate: Mapped[datetime] = Column(DateTime,nullable=True)
 
 class Mitigations(db.Model):
     __tablename__ = 'mitigations'
-    id: Mapped[int] = Column(primary_key=True)
+    id: Mapped[int] = Column(Integer,primary_key=True)
     description: Mapped[str] = Column(String(100), nullable=False)
     probability: Mapped[int] = Column(Integer)
     impact: Mapped[int] = Column(Integer)
     risk_id: Mapped[int] = Column(ForeignKey('risks.id'))
-    risk: Mapped['Risks'] = relationship("Risks",backref="mitigations")
+    risk: Mapped['Risks'] = relationship("Risks",back_populates="mitigations")
     date: Mapped[datetime] = Column(DateTime)
-    complete: Mapped[int] = Column(default=0)
+    complete: Mapped[int] = Column(Integer,default=0)
 
 class Programs(db.Model):
     __tablename__ = 'programs'
-    id: Mapped[int] = Column(primary_key=True)
+    id: Mapped[int] = Column(Integer,primary_key=True)
     name: Mapped[str] = Column(String(100))
     description: Mapped[str] = Column(String(100))
-    risks: Mapped[List["Risks"]] = relationship("Risks",backref="program")
+    risks: Mapped[List["Risks"]] = relationship("Risks",back_populates="program")
