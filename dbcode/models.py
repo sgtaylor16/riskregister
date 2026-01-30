b93f79638d89523672488150b474114431f651b4
Scott Taylor <sgtaylor16@gmail.com>
2026-01-29 22:06:52 -0500

    Refactored for sql 1.4

--- dbcode/models.py
+++ dbcode/models.py
@@ -1,48 +1,49 @@
-from sqlalchemy.orm import Mapped, relationship
-from sqlalchemy import ForeignKey, Column
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey, Column, Table
 from sqlalchemy import Integer, String, DateTime
+from sqlalchemy.orm import declarative_base
 from datetime import datetime
 from typing import List
 from extensions import db
 
-
-class Persons(db.Model):
+Base = declarative_base()
+class Persons(Base):
     __tablename__ = 'persons'
-    id: Mapped[int] = Column(Integer, primary_key=True)
-    last_name: Mapped[str] = Column(String(50),nullable=False)
-    first_name: Mapped[str] = Column(String(50),nullable=False)
-    risks: Mapped[List["Risks"]] = relationship("Risks",back_populates="person")
+    id = Column(Integer, primary_key=True)
+    last_name = Column(String(50),nullable=False)
+    first_name = Column(String(50),nullable=False)
+    risks = relationship("Risks",back_populates="person")
 
-class Risks(db.Model):
+class Risks(Base):
     __tablename__ = 'risks'
-    id: Mapped[int] = Column(Integer,primary_key=True)
-    ifstatement: Mapped[str] = Column(String(100))
-    thenstatement: Mapped[str] = Column(String(100))
-    probability: Mapped[int] = Column(Integer)
-    impact: Mapped[int] = Column(Integer)
-    person_id: Mapped[int] = Column(ForeignKey('persons.id'),nullable=True)
-    person: Mapped['Persons'] = relationship("Persons",back_populates="risks")
-    program_id: Mapped[int] = Column(ForeignKey('programs.id'))
-    program: Mapped["Programs"] = relationship("Programs",back_populates="risks")
-    mitigations: Mapped[List["Mitigations"]] = relationship("Mitigations",back_populates="risk")
-    date: Mapped[datetime] = Column(DateTime)
-    realizedate: Mapped[datetime] = Column(DateTime,nullable=True)
-    expiredate: Mapped[datetime] = Column(DateTime,nullable=True)
-
-class Mitigations(db.Model):
+    id = Column(Integer,primary_key=True)
+    ifstatement = Column(String(100))
+    thenstatement = Column(String(100))
+    probability = Column(Integer)
+    impact = Column(Integer)
+    person_id = Column(ForeignKey('persons.id'),nullable=True)
+    person = relationship("Persons",back_populates="risks")
+    program_id = Column(ForeignKey('programs.id'))
+    program = relationship("Programs",back_populates="risks")
+    mitigations = relationship("Mitigations",back_populates="risk")
+    date = Column(DateTime)
+    realizedate = Column(DateTime,nullable=True)
+    expiredate = Column(DateTime,nullable=True)
+    
+class Mitigations(Base):
     __tablename__ = 'mitigations'
-    id: Mapped[int] = Column(Integer,primary_key=True)
-    description: Mapped[str] = Column(String(100), nullable=False)
-    probability: Mapped[int] = Column(Integer)
-    impact: Mapped[int] = Column(Integer)
-    risk_id: Mapped[int] = Column(ForeignKey('risks.id'))
-    risk: Mapped['Risks'] = relationship("Risks",back_populates="mitigations")
-    date: Mapped[datetime] = Column(DateTime)
-    complete: Mapped[int] = Column(Integer,default=0)
+    id = Column(Integer,primary_key=True)
+    description = Column(String(100), nullable=False)
+    probability = Column(Integer)
+    impact = Column(Integer)
+    risk_id = Column(ForeignKey('risks.id'))
+    risk = relationship("Risks",back_populates="mitigations")
+    date = Column(DateTime)
+    complete = Column(Integer,default=0)
 
-class Programs(db.Model):
+class Programs(Base):
     __tablename__ = 'programs'
-    id: Mapped[int] = Column(Integer,primary_key=True)
-    name: Mapped[str] = Column(String(100))
-    description: Mapped[str] = Column(String(100))
-    risks: Mapped[List["Risks"]] = relationship("Risks",back_populates="program")
+    id = Column(Integer,primary_key=True)
+    name = Column(String(100))
+    description = Column(String(100))
+    risks = relationship("Risks",back_populates="program")
