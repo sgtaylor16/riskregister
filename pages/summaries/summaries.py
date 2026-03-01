af889785520f64e33aa8efbfaa464f6611b57a74
Scott Taylor <sgtaylor16@gmail.com>
2026-03-01 11:12:20 -0500

    Summaries page doesn't show archived risks

--- pages/summaries/summaries.py
+++ pages/summaries/summaries.py
@@ -11,7 +11,7 @@ summary_bp = Blueprint('summary', __name__)
 @summary_bp.route('/pigdata')
 def pigcount():
 
-    allrisks = db.session.execute(select(Risks)).scalars().all()
+    allrisks = db.session.execute(select(Risks).where(Risks.archive == 0)).scalars().all()
 
     countdf = pd.DataFrame(0,columns = [1,2,3,4,5],index = [1,2,3,4,5])
 
